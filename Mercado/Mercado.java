b0033015d86ae610efc2adfad2b250d090249415
Alexis Lopes <alexis.m753@gmail.com>
2017-09-21 18:09:06 -0300

    Add files via upload

---
+++ Mercado/Mercado.java
@@ -0,0 +1,19 @@
+package br.com.fatec;
+
+import java.util.Scanner;
+
+public class Mercado {
+    static Scanner lernum = new Scanner(System.in);
+    static Scanner lerstr = new Scanner(System.in);
+
+    public void intro() {
+        int option;
+        System.out.println("****************** LOJA ***************");
+        System.out.println("**   1 - ENTRAR   2 - CADASTRAR-SE   **");
+        System.out.println("***************************************");
+        System.out.println();
+
+    }
+
+
+}
