42ded1c8a7f4a4415aae06a4566566cc3dc4999f
Alexis Lopes <alexis.m753@gmail.com>
2017-09-21 18:38:48 -0300

    Create Usuario.java

---
+++ Mercado/Usuario.java
@@ -0,0 +1,25 @@
+package br.com.fatec;
+
+public class Usuário {
+
+    private String user;
+    private String senha;
+
+    public Usuário(String user, String senha) {
+        this.user = user;
+        this.senha = senha;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+ 
