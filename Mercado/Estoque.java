4992a319d9c31907d88ce4397959b5185411e5bd
ZeTheGreat <32179394+ZeTheGreat@users.noreply.github.com>
2017-09-21 19:24:06 -0300

    Update Estoque.java

--- Mercado/Estoque.java
+++ Mercado/Estoque.java
@@ -6,13 +6,45 @@ import java.util.Scanner;
 
 public class Estoque {
 	Scanner in= new Scanner(System.in);
-	private List<Produto> produto=new ArrayList<>();
+	private List<Produto> produtos=new ArrayList<>();
+	
+	public boolean verifNome(String nome) {
+		for(int c=0;c<produtos.size();c++)
+			if(produtos.get(c).equals(nome))
+				return false;
+		return true;
+	}
 	
 	public void  criarProduto() {
 		boolean c=true;
+		String nome="";
+		List<String> teg;
+		while(c) {
+			System.out.println("Digite o nome do Produto");
+			nome=in.next();
+			if(verifNome(nome)) {
+				System.out.println("Ja existe este produto!");
+				break;
+			}
+		}
+		teg=criarTeg();
+		System.out.println("Digite a quantidade");
+		int quantidade = in.nextInt();
+		System.out.println("Digite o preco");
+		float preco=in.nextFloat();
+		Produto prod= new Produto(nome,teg,quantidade,preco);
+		produtos.add(prod);
+	}
+	
+	public void excluirProduto(String nome) {
+		for(int c=0;c<produtos.size();c++)
+			if(produtos.get(c).equals(nome))
+				produtos.remove(c);
+	}
+	
+	public List<String> criarTeg() {
+		boolean c=true;
 		List<String> teg=new ArrayList<>() ;
-		System.out.println("Digite o nome do Produto");
-		String nome=in.next();
 		while(c) {
 			System.out.println("Digite as teg's a serem vinculadas");
 			teg.add(in.next());
@@ -21,10 +53,6 @@ public class Estoque {
 			if(resp!=1)
 				c=false;
 		}
-		System.out.println("Digite a quantidade");
-		int quantidade = in.nextInt();
-		System.out.println("Digite o preco");
-		float preco=in.nextFloat();
-		Produto prod= new Produto(nome,teg,quantidade,preco);
+		return teg;
 	}
 }
