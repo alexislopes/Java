aa1321708fe801e6364d442098d90958753f20d7
Alexis Lopes <alexis.m753@gmail.com>
2017-09-21 22:58:19 -0300

    v0.0.3
     ♥ v0.0.1 - Extensão à classe Usuario.
     ♥ v0.0.2 - Mudança do construtor para vazio.
     ♥ v0.0.3 - Adicionado o atributo codigo.

--- Mercado/Administrador.java
+++ Mercado/Administrador.java
@@ -1,7 +1,19 @@
 package br.com.fatec;
 
 public class Administrador extends Usuario {
-    public Administrador(String user, String senha) {
-        super(user, senha);
+    public Administrador() {
+
     }
+
+    private int codigo = 666;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+
 }
