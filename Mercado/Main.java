3fb518147428969945eba7940197df40752686c7
Alexis Lopes <alexis.m753@gmail.com>
2017-09-23 02:09:40 -0300

    Updated by alexis

--- Mercado/Main.java
+++ Mercado/Main.java
@@ -3,7 +3,8 @@ package br.com.fatec;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-	Mercado mercado = new Mercado();
-	mercado.intro();
+        
+        Mercado mercado = new Mercado();
+        mercado.intro();
     }
 }
