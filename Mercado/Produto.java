5a762761346c13acada868799d71eff1943a625a
Alexis Lopes <alexis.m753@gmail.com>
2017-09-23 02:15:07 -0300

    Updated by alexis

--- Mercado/Produto.java
+++ Mercado/Produto.java
@@ -1,4 +1,4 @@
-package Trab;
+package br.com.fatec;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,15 +8,14 @@ public class Produto {
     Scanner lernum = new Scanner(System.in);
     Scanner lerstr = new Scanner(System.in);
 
-    Estoque e = new Estoque();
+    //Mercado m = new Mercado();
 
-    private String id = "P";
-    private int intid;
+    private int id = 0;
     private String nome;
-    private List<String> teg;
+    private List<String> tegs;
     private int quantidade;
     private double preco;
-    private List<String> coment = new ArrayList<>();
+    private List<String> coments = new ArrayList<>();
 
     String verdim = "\033[36m";
     String amarelo = "\033[33m";
@@ -24,54 +23,12 @@ public class Produto {
     String verde = "\033[32m";
     String vermelho = "\033[31m";
 
-    public Produto(String nome, List<String> teg, int quantidade, double preco, int intid) {
+    public Produto(String nome, List<String> tegs, int quantidade, double preco, int intid) {
         this.id = id + intid;
         this.nome = nome;
-        this.teg = teg;
+        this.tegs = tegs;
         this.quantidade = quantidade;
         this.preco = preco;
-    }
-
-/////////////////////////////////////////// MÉTODOS PARA LISTAR COMENTÁRIOS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-    public void listarCommment() {
-        if (coment.isEmpty()) {
-            System.out.println(vermelho + "Ainda não há nenhumm comentário!" + limpo);
-        } else {
-            System.out.println(amarelo + "COMENTÁRIOS" + limpo);
-            for (String comment : coment) {
-                System.out.println(comment);
-            }
-        }
-    }
-
-///////////////////////////////////////////// MÉTODOS DE OPÇÕES DE PRODUTOS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-    public void opcoesDeProd() {
-        int option;
-        do {
-            System.out.println("********************** OPÇÕES DE PRODUTO *******************");
-            System.out.println("**   1 - COMPRAR   2 - COMENTAR   3 - VOLTAR   4 - INFO   **");
-            System.out.println("************************************************************");
-            option = lernum.nextInt();
-
-            switch (option) {
-                case 1:
-                    e.comprarProduto();
-                    break;
-
-                case 2:
-                    commentProd();
-                    break;
-
-                case 3:
-                    break;
-
-                case 4:
-                    e.informacaoProduto();
-                    break;
-            }
-        } while (option != 3);
     }
 
 //////////////////////////////// MÉTODO DE FORMATAÇÃO DE DADOS DO PRODUTO PARA LISTAGEM \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
@@ -88,38 +45,7 @@ public class Produto {
         return dadosFormatados;
     }
 
-///////////////////////////////////////////// MÉTODO PARA COMENTAR PRODUTOS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-    public void commentProd() {
-
-        String idProd;
-        String comment;
-
-        System.out.println("Digite o ID do produto: ");
-        idProd = lerstr.nextLine();
-        for (int i = 0; i < e.getProdutos().size(); i++) {
-            if (idProd.equals(e.getProdutos().get(i).getId())) {
-                System.out.println("Digite seu comentário: ");
-                comment = lerstr.nextLine();
-                e.getProdutos().get(i).coment.add(comment);
-            }
-        }
-        System.out.println(verdim + "Seu comentário foi adicionado ao produto, Obrigado!" + limpo);
-    }
-
-//////////////////////////////////////////// MÉTODO DE INFORMAÇÕES DO PRODUTO \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-    public void infoProd() {
-        System.out.print("Digite o ID do produto: ");
-        String id = lerstr.nextLine();
-        for (int i = 0; i < e.getProdutos().size(); i++) {
-            if (e.getProdutos().get(i).getId().equals(id)) {
-                System.out.println(e.getProdutos().get(i).dadosFormatados());
-                e.getProdutos().get(i).listarCommment();
-            }
-        }
-
-    }
 
 
     public String getNome() {
@@ -130,12 +56,12 @@ public class Produto {
         this.nome = nome;
     }
 
-    public List<String> getTeg() {
-        return teg;
+    public List<String> getTegs() {
+        return tegs;
     }
 
-    public void setTeg(List<String> teg) {
-        this.teg = teg;
+    public void setTegs(List<String> tegs) {
+        this.tegs = tegs;
     }
 
     public int getQuantidade() {
@@ -150,34 +76,34 @@ public class Produto {
         return preco;
     }
 
-    public void setPreco(float preco) {
+    public void setPreco(double preco) {
         this.preco = preco;
     }
 
-    public List<String> getComent() {
-        return coment;
+    public List<String> getComents() {
+        return coments;
     }
 
-    public void setComent(List<String> coment) {
-        this.coment = coment;
+    public void setComents(List<String> coments) {
+        this.coments = coments;
     }
 
     public void addComent(String coment) {
-        this.coment.add(coment);
+        this.coments.add(coment);
     }
 
     public void removeComent(int posi) {
-        this.coment.remove(posi);
+        this.coments.remove(posi);
     }
 
     public void addTeg(String teg) {
-        this.teg.add(teg);
+        this.tegs.add(teg);
     }
 
     public void decressTeg(String nome) {
-        for (int i = 0; i < teg.size(); i++)
-            if (teg.get(i).equals(nome))
-                teg.remove(i);
+        for (int i = 0; i < tegs.size(); i++)
+            if (tegs.get(i).equals(nome))
+                tegs.remove(i);
     }
 
     public void addQuantidade(int qnt) {
@@ -188,13 +114,15 @@ public class Produto {
         this.quantidade -= qnt;
     }
 
-    public String getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(int id) {
         this.id = id;
     }
+
+
 
 
 }
