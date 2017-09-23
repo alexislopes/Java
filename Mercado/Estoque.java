13f07a086f6c90fe4bbe9c193602a00f035dd004
Alexis Lopes <alexis.m753@gmail.com>
2017-09-23 02:14:20 -0300

    Update by alexis

--- Mercado/Estoque.java
+++ Mercado/Estoque.java
@@ -1,20 +1,23 @@
-package Trab;
+package br.com.fatec;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Estoque {
+    Produto novo;
+    Mercado mercado;
+    private List<Produto> produtos = new ArrayList<>();
 
     String verdim = "\033[36m";
     String limpo = "\033[m";
     String vermelho = "\033[31m";
     String verde = "\033[32m";
+    String amarelo = "\033[33m";
 
-    Scanner in = new Scanner(System.in);
-    Scanner innum = new Scanner(System.in);
-    private List<Produto> produtos = new ArrayList<>();
-    Produto novo;
+    Scanner lerstr = new Scanner(System.in);
+    Scanner lernum = new Scanner(System.in);
+
 
 //////////////////////////////////// MÉTODOS PARA VERIFICAR EXISTENCIA DE UM PRODUTO \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
@@ -26,33 +29,10 @@ public class Estoque {
         return false;
     }
 
-    /*public void criarProduto() {
-        boolean c = true;
-        String nome = "";
-        List<String> teg;
-        while (c) {
-            System.out.print("Digite o nome do Produto: ");
-            nome = in.next();
-            if (verifNome(nome)) {
-                System.out.println(vermelho + "Ja existe este produto!" + limpo);
-                break;
-            }
-        }
-        teg = criarTeg();
-        System.out.print("Digite a quantidade: ");
-        int quantidade = in.nextInt();
-        System.out.print("Digite o preco: ");
-        float preco = in.nextFloat();
-        Produto prod = new Produto(nome, teg, quantidade, preco);
-        produtos.add(prod);
-        System.out.println(verdim + "\nProduto cadastrado com sucesso!" + limpo);
-    }*/
 
 //////////////////////////////////////////// MÉTODOS PARA ADICIONAR PRODUTOS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-// Método atualizado para add produtos.
 
-
-    int intid = 0;
+    private int intid = 0;
 
     public void addProduto() {
         String nome;
@@ -66,27 +46,27 @@ public class Estoque {
                 System.out.println(vermelho + "Esse produtp já existe!\n" + limpo);
             }
             System.out.print("Digite um nome para o produto: ");
-            nome = in.nextLine();
+            nome = lerstr.nextLine();
             valid = verifNome(nome);
         } while (valid);
         System.out.print("Digite a quantidade do produto: ");
-        int quantidade = innum.nextInt();
+        int quantidade = lernum.nextInt();
 
-        System.out.print("Digite  o preço do produto: ");
-        double preco = innum.nextDouble();
+        System.out.print("Digite o preço do produto: ");
+        double preco = lernum.nextDouble();
 
         int option;
         System.out.println("Deseja adidcionar algumas tags? ");
         System.out.print("\t1 - SIM    2 - NÃO");
-        option = innum.nextInt();
+        option = lernum.nextInt();
         if (option == 1) {
             String tag;
             int qtdade;
             System.out.print("Quantas? ");
-            qtdade = innum.nextInt();
+            qtdade = lernum.nextInt();
             for (int i = 0; i < qtdade; i++) {
                 System.out.print("Digite a tag: ");
-                tag = in.nextLine();
+                tag = lerstr.nextLine();
                 tags.add(tag);
             }
         } else {
@@ -96,97 +76,115 @@ public class Estoque {
         novo = new Produto(nome, tags, quantidade, preco, intid);
         produtos.add(novo);
         System.out.println(verdim + "\nProduto cadastrado com sucesso!\n" + limpo);
-
-
-    }
-///////////////////////////////////////////// COMPRAR PRODUTO \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-    
-    public List<Double> comprarProduto() {
-    	List<Double> preco=new ArrayList<>();
-    	System.out.println("Digite o ID do produto:");
-    	int ID=innum.nextInt();
-    	System.out.println("Digite a quantidade:");
-    	int qtd=innum.nextInt();
-    	boolean resp=verificarQuantidade(ID,qtd);
-    	if(resp) {
-    		produtos.get(ID).setQuantidade(produtos.get(ID).getQuantidade()-qtd);
-    		preco.add(gerarPreco(ID,qtd));
-    		double aux=(double) ID;
-    		preco.add(aux);
-    		return preco;
-    	}
-    	else {
-    		System.out.println("Quantidade de estoque insuficiente!");	
-    		return preco;
-    	}
-    }
-    
-///////////////////////////////////////////// INFORMACAO DO PRODUTO \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-    
-    public void informacaoProduto() {
-    	 System.out.print("Digite o ID do produto: ");
-         int id = innum.nextInt();
-         System.out.println(produtos.get(id).getPreco());
-         System.out.println(produtos.get(id).getNome());
-         System.out.println(produtos.get(id).getTeg());
-         System.out.println(produtos.get(id).getQuantidade());
-    }
-    
-///////////////////////////////////////////// VERIFIVAR QUANTIDADE \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-    
-    public boolean verificarQuantidade(int id,int qtd) {
-    	int qtdn=produtos.get(id).getQuantidade();
-    	if(qtd>qtdn)
-    		return true;
-    	return false;
-    }
-   
-///////////////////////////////////////////// GERAR PRECO \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-    
-    public double gerarPreco(int id, int qtd) {
-    	double result=0;
-    	result= (produtos.get(id).getPreco())*qtd;
-    	return result;
-    }
-    
-///////////////////////////////////////////// MÉTODOS PARA EXCLUIR PRODUTOS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-    public void excluirProduto(String nome) {
-        for (int c = 0; c < produtos.size(); c++)
-            if (produtos.get(c).getNome().equals(nome))
-                produtos.remove(c);
     }
 
-//////////////////////////////////////////////// MÉTODOS PARA CRIAR TAG \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-// no método addProduto(), há uma seção para adição de tags.
 
-    public List<String> criarTeg() {
-        boolean c = true;
-        List<String> teg = new ArrayList<>();
-        while (c) {
-            System.out.println("Digite as teg's a serem vinculadas");
-            teg.add(in.next());
-            System.out.println("Deseja vincular outra teg? 1-sim/qualquer outro numero-nao");
-            int resp = in.nextInt();
-            if (resp != 1)
-                c = false;
-        }
-        return teg;
-    }
+////////////////////////////////////////////// VERIFICAR QUANTIDADE \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-///////////////////////////////////////////// MÉTODOS PARA LISTAR PRODUTOS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    public boolean verificarQuantidade(int id, int qtd) {
+        int qtdn;
 
-    public void listarProdutos() {
-        if (produtos.isEmpty()) {
-            System.out.println(verde + "Ainda não há nenhum produto\n" + limpo);
-        } else {
-            for (Produto produto : produtos) {
-                System.out.println(produto.getId() + " - " + produto.getNome());
+        for (Produto produto : produtos)
+            if (id == produto.getId()) {
+                qtdn = produto.getQuantidade();
+                if (qtd <= qtdn) {
+                    return true;
+                } else {
+                    return false;
+                }
             }
-            novo.opcoesDeProd();
+        return false;
+    }
+
+///////////////////////////////////////////// MÉTODOS PARA VARRER PRODUTOS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+    public void varrerProduto() {
+        for (int i = 0; i < produtos.size(); i++) {
+            if (produtos.get(i).getQuantidade() == 0) {
+                produtos.remove(i);
+            }
         }
     }
 
+///////////////////////////////////////////// MÉTODO PARA EDITAR PRODUTOS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+    public void editarProduto() {
+
+        System.out.println("****************** EDITAR QUAL ATRIBUTO? *******************");
+        System.out.println("**   1 - NOME   2 - QUANTIDADE   3 - PREÇO   4 - VOLTAR   **");
+        System.out.println("************************************************************");
+        int option = lernum.nextInt();
+
+        switch (option) {
+            case 1:
+                editarNome();
+                break;
+
+            case 2:
+                editarQtd();
+                break;
+
+            case 3:
+                editarPreco();
+                break;
+
+            case 4:
+                break;
+
+
+        }
+    }
+
+
+////////////////////////////////////////// MÉTODO PARA EDITAR NOME DO PRODUTO \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+    public void editarNome() {
+        System.out.println("Digite o ID do produto: ");
+        int idprod = lernum.nextInt();
+        for (Produto produto : produtos) {
+            if (idprod == produto.getId()) {
+                System.out.println("Novo nome para " + produto.getNome() + ":");
+                String nome = lerstr.nextLine();
+                produto.setNome(nome);
+                System.out.println(verdim + "Nome alterado para: " + produto.getNome() + limpo);
+            }
+        }
+
+    }
+
+////////////////////////////////////// MÉTODO PARA EDITAR QUANTIDADE DO PRODUTO \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+    public void editarQtd() {
+        System.out.println("Digite o ID do produto: ");
+        int idprod = lernum.nextInt();
+        for (Produto produto : produtos) {
+            if (idprod == produto.getId()) {
+                System.out.println("Nova quantidade para " + produto.getNome() + ":");
+                int quantidade = lerstr.nextInt();
+                produto.setQuantidade(quantidade);
+                System.out.println(verdim + "Quanridade alterada para: " + produto.getQuantidade() + limpo);
+            }
+        }
+
+    }
+
+///////////////////////////////////////// MÉTODO PARA EDITAR PRECO DO PRODUTO \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+    public void editarPreco() {
+        System.out.println("Digite o ID do produto: ");
+        int idprod = lernum.nextInt();
+        for (Produto produto : produtos) {
+            if (idprod == produto.getId()) {
+                System.out.println("Novo preço para " + produto.getNome() + ":");
+                double preco = lernum.nextDouble();
+                produto.setPreco(preco);
+                System.out.println(verdim + "Preço alterado para: " + produto.getPreco() + limpo);
+            }
+        }
+
+    }
+
+///////////////////////////////////////// ################################### \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 
     public void setProdutos(List<Produto> produtos) {
